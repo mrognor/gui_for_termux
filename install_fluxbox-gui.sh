28e5d0f256ca9d31f4cb9229c253de6a990f6a3b
mrognor <47296449+mrognor@users.noreply.github.com>
2020-11-29 13:56:07 +0300

    Update install_fluxbox-gui.sh
    additional fixing

--- install_fluxbox-gui.sh
+++ install_fluxbox-gui.sh
@@ -4,7 +4,8 @@ vncserver -localhost
 echo "you have to set your vnc password and confirm it"
 vncserver -kill localhost:1
 pkg install fluxbox -y
-echo "#!/data/data/com.termux/files/usr/bin/sh > ~/.vnc/xstartup
+echo "#!/data/data/com.termux/files/usr/bin/sh" > ~/.vnc/xstartup
 echo "## Fluxbox desktop. # Generate menu. fluxbox-generate_menu" >> ~/.vnc/xstartup
 echo "# Start fluxbox." >> ~/.vnc/xstartup
 echo "fluxbox &" >> ~/.vnc/xstartup
+vncserver
